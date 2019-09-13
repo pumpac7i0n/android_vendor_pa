922c96990caa15942762c9ea5a1601deeebb965d
Jake Weinstein <jake@aospa.co>
2019-09-13 06:39:29 +0200

    [DNM] vendor: add QSSI
    Change-Id: I552bc498fc7c77dbd964d76cf2107124037c6918

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -26,6 +26,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus7pro/pa_oneplus7pro.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
+    $(LOCAL_DIR)/qssi/pa_qssi.mk \
 
 # SONY OpenDevices
 # Loire 8956
@@ -55,5 +56,6 @@ COMMON_LUNCH_CHOICES += \
     pa_oneplus6-userdebug \
     pa_oneplus7pro-userdebug \
     pa_polaris-userdebug \
+    pa_qssi-userdebug \
     pa_sagit-userdebug \
     pa_suzu-userdebug
